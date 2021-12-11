e54edcce7cb4df798226df85448d7e1d2c3db622
Milan G <dev@milangress.de>
2021-12-11 18:04:51 +0100

    ✨ add blue sky

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,20 @@
-<template>
-  <Tutorial/>
+<template lang="pug">
+  .index-container
+
+
 </template>
 
 <script>
 export default {}
 </script>
+
+<style>
+
+.index-container {
+  height: 100vh;
+  width: 100%;
+  background-image: url("/blauer-himmel-scale-2_00x.jpeg");
+  background-size: cover;
+  background-position: 50% 50%;
+}
+</style>
