aca036e292481c2b571a6b1540550149962ba57b
Milan G <dev@milangress.de>
2022-01-14 17:49:19 +0100

    ✨ header font

---
+++ components/pages/home/HeaderHome.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+  .header-container
+    .headline
+      h1 Highly <br> Speculative <br> Design <br> Fiction
+    MatrixSVG2d
+</template>
+
+<script>
+export default {
+  name: "HeaderHome"
+}
+</script>
+
+<style scoped>
+.header-container {
+  height: 100vh;
+  display: grid;
+}
+.header-container > * {
+  grid-column: 1;
+  grid-row: 1;
+}
+.headline {
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 6.5vw;
+  line-height: 1;
+  transform: scaleX(1.25);
+  color: #ff0000;
+  letter-spacing: -0.05em;
+  text-shadow: 0 0 6px #fff, 0 0 16px #fff, 0 0 26px #fff, 0 0 36px #fff;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,21 @@
 <template lang="pug">
   .index-container
     BackgroundBlur
-    .header-container
-      MatrixSVG2d
+    HeaderHome
     .content
       MatrixSVG2d
 
 </template>
 
 <script>
-export default {}
+import HeaderHome from "~/components/pages/home/HeaderHome"
+export default {
+  components: {HeaderHome}
+}
 </script>
 
 <style>
 
-
-.header-container {
-  height: 100vh;
-}
 .content {
   height: 100vh;
 }
