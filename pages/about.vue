9729b3a0aa4e387bdf9bba20f5d6c795355e2a31
Milan G <dev@milangress.de>
2022-01-15 15:13:05 +0100

    ✨ about

---
+++ pages/about.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  .about-page
+</template>
+
+<script>
+export default {
+  name: "about"
+}
+</script>
+
+<style scoped>
+
+.about-page {
+  min-height: 500vh;
+  background-color: #ff0000;
+  background-image: url("/about_bg.gif");
+  background-repeat: repeat-y;
+  background-size: 100%;
+  background-position: center;
+  filter: blur(20px);
+}
+
+</style>
