5e80426efb880c7c943ef1e54bc0947044fc15cf
Milan G <dev@milangress.de>
2022-02-26 14:26:58 +0100

    update text

--- components/pages/home/ProjectLinks.vue
+++ components/pages/home/ProjectLinks.vue
@@ -1,13 +1,13 @@
 <template lang="pug">
   .project-links
     .box
-      p »Highly Speculative Design Fiction« is a research project—somewhere in the blurry field between design and art— Milan Gress and I started this project during our studies at Hochschule Mainz  [DE] and our joint bachelor project originated under the same name/idea and is still in progress today...with »Highly Speculative Design Fiction« is a research project—somewhere in the blurry field between design and art— Milan Gress and I started this project during our studies at Hochschule Mainz  [DE] and our joint bachelor project originated under the same name/idea and is still in progress today...with
+      p The FaaS-Maschine (Future as a Service) is part of our finals and was created as a tool to aid our creative process. Our research was based around finding new ways to take back control over the future and democratizing the process of future speculation. The sentences are based on hand-picked sentence fragments in a Google sheet. The fragments are combined at random and sometimes create vague as well as very specific statements about the future. Thereby allowing one to think in new and unexpected ways about the future.
       .button-bottom
-        BlurButton(to="https://faas.unreal.enterprises/" bg-color="rgb(0,255,0)" extern) Future as a Service <br> FAAS-Machine
+          BlurButton(to="https://faas.unreal.enterprises/" bg-color="rgb(0,255,0)" extern) Future as a Service <br> FAAS-Machine
     .box
-      p »Highly Speculative Design Fiction« is a research project—somewhere in the blurry field between design and art— Milan Gress and I started this project during our studies at Hochschule Mainz  [DE] and our joint bachelor project originated under the same name/idea and is still in progress today...with »Highly Speculative Design Fiction« is a research project—somewhere in the blurry field between design and art— Milan Gress and I started this project during our studies at Hochschule Mainz  [DE] and our joint bachelor project originated under the same name/idea and is still in progress today...with
+      p The HSDF-Matrix (Highly Speculative Design Fiction) was part of our finals and the main tool we created for developing new future scenarios. We mapped different relevant social tendencies into 3D space and formed a matrix where different scenarios could live side by side, therefore emphasizing that the future is a movable and actionable space. The HSDF-Page was used to decipher and communicate this abstract future space. With the possibility to change and edit the axis and quadrants, we also created a way to dynamically use our matrix for collaborative future making in a workshop setting.
       .button-bottom
-        BlurButton(to="https://matrix.unreal.enterprises/" bg-color="rgb(252,11,11)" extern) HSDF <br> Matrix
+          BlurButton(to="https://matrix.unreal.enterprises/" bg-color="rgb(252,11,11)" extern) HSDF <br> Matrix
 
 </template>
 
