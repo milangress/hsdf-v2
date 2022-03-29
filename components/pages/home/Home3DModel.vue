21b57c8523f980489a3b7eaa83107385b66228e1
Milan G <dev@milangress.de>
2022-03-29 21:08:58 +0200

    config 3d viewer

--- components/pages/home/Home3DModel.vue
+++ components/pages/home/Home3DModel.vue
@@ -1,5 +1,10 @@
 <template>
-  <model-gltf src="/3dmodel.glb" background-alpha="0"></model-gltf>
+  <model-gltf src="/3dmodel.glb" background-alpha="0"
+              :controls-options="{
+            enablePan: false,
+            enableZoom: false,
+            enableRotate: true,
+          }"></model-gltf>
 </template>
 <script>
 import { ModelGltf } from 'vue-3d-model';
