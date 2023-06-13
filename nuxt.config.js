ef6524df86fb93bf7d37d0fb8b5ac3542f93579a
Milan G <dev@milangress.de>
2023-06-13 20:45:27 +0200

    title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'hdsf-v2',
+    title: 'Unreal Enterprises',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
