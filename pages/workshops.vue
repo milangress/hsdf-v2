4a3b7f438785d7438bd606c595e7ec8077c8e49b
Milan G <dev@milangress.de>
2023-06-23 14:52:21 +0200

    ws path

--- pages/workshops.vue
+++ pages/workshops.vue
@@ -1,20 +1,14 @@
 <template lang="pug">
 .text
-  h1 Highly Speculative Design Fiction
-  p Bachelor Thesis 2021
-  h2 Werk: Microscopic Intimacy
-  p <i>*Microscopic Intimacy*</i> ein Sexspielzeug, dass einem Szenario existiert, in dem das Bedürfnis nach Intimität externalisiert wird, weil biologische Körper als unrein empfunden werden. MI hilft bei der Re*sensibilisierung von Körper*bedürfnissen. Natürlich ohne die immer noch viel zu absurde echte menschliche Körper*Körper Berührung!
-  p Das Objekt kann auf zwei Ebenen aktiv berührt werden — Der am Computer fabrizierten Bewegung, in Linien abstrahiert und als Pfeile visualisiert – steht die natürliche Berührung und Bewegung mit ihren körperähnlichen, hervorstehenden Reliefstrukturen gegenüber. Die Formen sind abgeleitet von *Körperbildern*, die durch die Dokumentation von gegenseitigen Berührungen/ Massagen am ganzen Körper mit Farbe, Öl und Parfüm entstanden sind.
-  h2 Werk: Visionary Imaginary Landscape Project
-  p Das Szenario *Visionary Imaginary Landscape Project* untersucht wie das Leben in einer komfortablen, schönen Illusion (X) Aspekte fortschreitenden Klimawandel *überdeckt*. Es positioniert [Design als *Oberfläche*] als eine Kraft gegen die Natur und stellt die Frage inwiefern Design Fortschritt verhindern kann.
-  h2 Werk: Vitreous Body
-  p Die Arbeit *Vitreous Body* zeigt einen von zwei oppositionellen Gruppen gehackten Papiertuchspender. Das Szenario springt an den Punkt *Point of Vitreous Body* — dem Moment an dem mehr Kameras mit Eigenschaften, die die eines menschlichen Auges spiegeln (?)
-  p The *Point of Vitreous Body* has passed; The one moment in which more cameras whose capabilities mirror those of human eyes {*computerized eyes*} are looking at the world then human eyes. We created two fictional counter cultures that *hacked* a fictional toilet paper dispenser with a facial recognition system. The opposed messages are asking us: *How can human bodies process the omnipresent computerized eyes?* as well as: *How can we become fully transparent and disappear for the computerized eyes?* *Group A* hacked the spender to put a video on display, which shows the processing attempt of humanized, computerized eyes in a kitchen. *Group B* placed five differently designed pamphlets from the fictitious* Transparency Workbook* between the paper towels, ready to be taken out by the viewer. We play with the exaggerated impossibility inherent in both approaches. The central question of whether the human body fundamentally belongs to the private or the public sphere and how we as humans must be explored.
+  h1 Workshops
+  img(src="/workshops/class-structure.png")
+  img(src="/workshops/tool-ws.png")
+
 </template>
 
 <script>
 export default {
-  name: "hsdf",
+  name: "workshops",
   layout (context) {
     return 'textPage'
   }
